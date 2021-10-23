2e158296de6594a12b5471558cf9719c4552f821
A-J-M-A-L <88123331+Ajmal342@users.noreply.github.com>
2021-10-23 14:04:47 +0530

    Update and rename zara.js to VAVA.js

--- VAVA.js
+++ VAVA.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
+ V_NUMBER: process.env.NUMBER === undefined ? '+994407257213' : process.env.NUMBER,
+ V_OWNER: process.env.OWNER_NAME === undefined ? 'A-J-M-A-L' : process.env.OWNER_NAME,
 
 };
